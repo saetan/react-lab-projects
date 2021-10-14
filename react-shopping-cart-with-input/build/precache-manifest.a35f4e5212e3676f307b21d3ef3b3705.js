c01f90e48a94a3dd3a17238a34535257c5d8b368
saetan <saetan@live.com>
2021-10-15 00:35:05 +0800

    added shopping-cart-with-input

---
+++ react-shopping-cart-with-input/build/precache-manifest.a35f4e5212e3676f307b21d3ef3b3705.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbfca08f445b01e33c292b5c1aefdfc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f909bc5d817114ce9fa",
+    "url": "./static/css/main.c506c6ee.chunk.css"
+  },
+  {
+    "revision": "2ceef9c060bc6f2da1f2",
+    "url": "./static/js/2.33ec61b0.chunk.js"
+  },
+  {
+    "revision": "2f909bc5d817114ce9fa",
+    "url": "./static/js/main.00f874b7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
