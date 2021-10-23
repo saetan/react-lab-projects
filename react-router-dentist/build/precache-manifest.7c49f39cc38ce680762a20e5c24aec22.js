0ec0ed7ae6d0a6a7af64d6d5890f2797b675df8b
saetan <saetan@live.com>
2021-10-23 13:40:54 +0800

    change homepage

---
+++ react-router-dentist/build/precache-manifest.7c49f39cc38ce680762a20e5c24aec22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed36d8244c78de51994ea9caeda79cb0",
+    "url": "/react-lab-projects/react-router-dentist/build/index.html"
+  },
+  {
+    "revision": "8b2162a07b947b1ac5e3",
+    "url": "/react-lab-projects/react-router-dentist/build/static/css/main.3be36971.chunk.css"
+  },
+  {
+    "revision": "82766dad38323193035a",
+    "url": "/react-lab-projects/react-router-dentist/build/static/js/2.39205c72.chunk.js"
+  },
+  {
+    "revision": "8b2162a07b947b1ac5e3",
+    "url": "/react-lab-projects/react-router-dentist/build/static/js/main.7f21cb20.chunk.js"
+  },
+  {
+    "revision": "4ceaf7e992d6b0e6edaa",
+    "url": "/react-lab-projects/react-router-dentist/build/static/js/runtime~main.746cd2a2.js"
+  }
+]);
